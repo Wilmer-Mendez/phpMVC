41fc8260ec41a46e2a9cae23b5b5b3ed2809a275
Wilmer-Mendez <47387782+Wilmer-Mendez@users.noreply.github.com>
2020-04-07 17:45:35 +0800

    Add files via upload

---
+++ phpMVC/app/Core.php
@@ -0,0 +1,23 @@
+<?php
+    /*
+     *
+     * App Core Class
+     * Creates URL & loads core controller
+     * URL FORMAT - controller/method/params
+     * 
+     */
+    class Core {
+        protected $currentController = 'Pages';
+        protected $currentMethod = 'index';
+        protected $params = [];
+
+        public function __construct(){
+            $this->getURL();
+        }
+
+        public function getURL(){
+            echo $_GET['url'];
+        }
+    }
+
+    
